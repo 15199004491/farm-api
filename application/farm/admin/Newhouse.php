46e5017cfaebc79ae2e514beac9388264841a9e6
tb9004491_22 <wuchunyan@yiliyili.com.cn>
2024-08-16 00:41:55 +0800

    增加新房功能

---
+++ application/farm/admin/Newhouse.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace app\farm\admin;
+
+use app\farm\model\NewHouse as NewHouseModel;
+use app\common\controller\Common;
+
+/**
+ * 
+ * @package
+ */
+class NewHouse extends Common
+{
+    // 删除信息
+    public function deleteNewHouse()
+    {
+        $data = $this->request->param();
+        
+        $result = NewHouseModel::where('Id', $data['Id'])->delete();
+        
+        return $this->json_return($result);
+    }
+    /**
+     * 当前人发布的新房列表
+     */
+    public function newHouseSelf()
+    {
+        $data = $this->request->param();
+        $publisher = $data['publisher'];
+
+        // 默认信息查询条件
+        $map_data = [
+            ['publisher', '=', $publisher],
+        ];
+        
+        $data_list = NewHouseModel::where($map_data)->select();
+
+        return $this->json_result($data_list, 200, '操作成功');
+    } 
+    /**
+     * 新房发布
+     */
+    public function addNewHouse()
+    {
+        $data = $this->request->param();
+        $data['update_time'] = time();
+        if(isset($data['Id'])) {
+            $param = NewHouseModel::where('Id', $data['Id'])->update($data);
+        } else {
+            $param = NewHouseModel::insertGetId($data);
+        }
+       
+        return $this->json_return($param);
+    }
+    /**
+     * 查看新房信息
+     */
+    public function newHouseDetail()
+    {
+        $data = $this->request->param();
+        $result = NewHouseModel::where('Id', $data['Id'])->find();
+        return $this->json_return($result);
+    }
+     /**
+     * 新房列表
+     */
+    public function newHouseList()
+    {
+        $data = $this->request->param();
+        $keyword = $data['keyword'];
+
+        // 默认信息查询条件
+        $map_data = [
+            ['area', '=', $data['area']],
+            ['name|explain', 'like', "%$keyword%"]
+        ];
+        
+        $data_list = NewHouseModel::where($map_data)->order('update_time desc')->limit($data['start']-1, $data['end'])->select();
+
+        return $this->json_result($data_list, 200, '操作成功');
+    } 
+    // 置顶信息
+    public function topHouse()
+    {
+        $data = $this->request->param();
+        
+        $result = NewHouseModel::where('Id', $data['Id'])->update(['top_start' => $data['top_start'],'top_end' => $data['top_end']]);
+        
+        return $this->json_return($result);
+    }
+}
---
+++ application/farm/model/NewHouse.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace app\farm\model;
+
+use think\Model;
+
+/**
+ * 活动模型
+ * @package app\farm\model
+ */
+class NewHouse extends Model
+{
+    // 设置当前模型对应的完整数据表名称
+    protected $name = 'farm_new_house';
+
+    // 自动写入时间戳
+    protected $autoWriteTimestamp = true;
+
+}
